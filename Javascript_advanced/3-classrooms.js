73e233d0e34b879a6f7d7eb3e4eefb4fa1c2e296
Douglas <wdmdavison@icloud.com>
2023-07-08 22:51:13 -0500

    push 3 after testing

---
+++ Javascript_advanced/3-classrooms.js
@@ -0,0 +1,11 @@
+function createClassRoom(numberOfStudents) {
+    function studentSeat(seat) {
+        return () => {return seat;};
+    }
+    let students = [];
+    for (let i = 0; i < numberOfStudents; i++) {
+        students.push(studentSeat(i+1));
+    }
+    return students;
+}
+const classRoom = createClassRoom(10);
